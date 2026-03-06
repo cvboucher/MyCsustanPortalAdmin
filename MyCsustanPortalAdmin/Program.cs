e4d146c21f857942980422f0a1ce14b51f95bc83
Craig Boucher <cboucher@csustan.edu>
2026-03-06 12:31:26 -0800

    Initial commit

---
+++ MyCsustanPortalAdmin/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using MyCsustanPortalAdmin.Components;
+using MyCsustanPortalAdmin.Data;
+using Radzen;
+
+namespace MyCsustanPortalAdmin
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddRadzenComponents();
+
+            builder.Services.AddDbContextFactory<MyCsustanContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("MyCsustanContext")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+            app.UseHttpsRedirection();
+
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
